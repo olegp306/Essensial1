72ea6c5fc22c8e7bac84319ee30e342e21abda52
olegp306 <olegp306@gmail.com>
2017-01-15 11:58:21 +0600

    intitial statement 2

---
+++ Essential1/Essential1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+
+            Console.WriteLine("Введите ширину:");
+            double.TryParse(Console.ReadLine(), out a);
+
+            Console.WriteLine("Введите длину:");
+            double.TryParse(Console.ReadLine(), out b);
+
+            Rectangle rectangle = new Rectangle(a, b);
+
+            Console.WriteLine("Периметр прямогуальника равен: {0}", rectangle.Perimeter);
+
+            Console.WriteLine("Площадь прямогуальника равена: {0}", rectangle.Area);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Essential1/Essential1/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential1
+{
+    class Rectangle
+    {
+        double side1;
+        double side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        double AreaCalculator()
+        {
+            return side2 * side2;
+        }
+        double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+
+        //Создать два свойства double Area и double Perimeter с одним методом доступа get.
+        public double Area
+        {
+            get { return AreaCalculator(); }
+        }
+        public double Perimeter
+        {
+            get { return PerimeterCalculator(); }
+        }
+    }
+}

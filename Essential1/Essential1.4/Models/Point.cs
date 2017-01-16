a2509dc80df1cb5eef2d3e2106cbc82c878ab139
olegp306 <olegp306@gmail.com>
2017-01-16 09:30:01 +0600

    add Ess1.3

---
+++ Essential1/Essential1.4/Models/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential1._4
+{
+    class Point
+    {
+        int latitude;
+        int longtitude;
+        string name;
+
+        public int Latitude
+        {
+            get;
+            set;
+        }
+
+        public int Longtitude
+        { 
+            get;
+            set;
+        }
+
+
+    }
+
+}

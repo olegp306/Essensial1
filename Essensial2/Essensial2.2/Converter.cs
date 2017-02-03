65017f9cb00654f88f2986a096a0eb91105088db
olegp306 <olegp306@gmail.com>
2017-02-03 18:22:34 +0700

    middle commit

---
+++ Essensial2/Essensial2.2/Converter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essensial2._2
+{
+    public class Converter
+    {
+        readonly double UsdRate;
+        readonly double EuroRate;
+        readonly double RusRate;
+        readonly double GrivnaRate;
+
+        public double Rub;
+
+        public Converter(double usdRate, double euroRate, double grivnaRate)
+        {
+            this.UsdRate = usdRate;
+            this.EuroRate = euroRate;
+            this.GrivnaRate = grivnaRate;
+        }
+
+      
+
+        void ShowCurrencyInRuble()
+        {
+            Console.WriteLine("У вас {0}рублей  это {1}долларов,{2}евро, {3}гривен", Rub, Rub/UsdRate, Rub / EuroRate, Rub / GrivnaRate);
+        }
+
+        void ShowRubleInCurrency()
+        {
+            Console.WriteLine("У вас {0}рублей  это {1}долларов,{2}евро, {3}гривен", Rub, Rub / UsdRate, Rub / EuroRate, Rub / GrivnaRate);
+        }
+
+    }
+}

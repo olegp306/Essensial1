77ba1643de4fc4987cd4dcae6809c7677864baa7
olegp306 <olegp306@gmail.com>
2017-03-07 13:25:07 +0700

    add home work essential 2.4

---
+++ Essensial2/Essensial2.4/Invoice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essensial2._4
+{
+    public class Invoice
+    {
+        //        Задание 4 
+        //Используя Visual Studio, создайте проект по шаблону Console Application.
+        //Требуется:  
+        //Создать класс Invoice.
+        //В теле класса создать три поля int account, string customer, string provider которые должны быть проинициализированы один раз (при создании экземпляра данного класса) без возможности их дальнейшего изменения.
+        //В теле класса создать два закрытых поля string article, int quantity
+        //Создать метод расчета стоимости заказа с НДС и без НДС.
+        //Написать программу, которая выводит на экран сумму оплаты заказанного товара с НДС или без НДС
+        const double ndsAmount = 13;
+
+        readonly int account;
+        readonly string customer;
+        readonly string provider;
+
+        string article;
+        int quantity;
+
+        public string Article
+        {
+            get
+            {
+                return article;
+            }
+            set
+            {
+                article = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        public void ShowOrder(double cost)
+        {
+            Console.WriteLine("Оформение заказа !!!");
+            Console.WriteLine("Аккаунт- {0} ", account);
+            Console.WriteLine("Покупатель- {0} ", customer);
+            Console.WriteLine("Организация по доставке - {0} ", provider);
+
+            Console.WriteLine("Вы заказали {0} в кол-ве {1} штук", Article, Quantity);
+
+            Console.WriteLine("Сумма БЕЗ НДС составит {0}", CalculateOrder(false, cost));
+            Console.WriteLine("Сумма с НДС составит {0}", CalculateOrder(true, cost));
+        }
+
+        double CalculateOrder(bool withNds, double cost)
+        {
+            if (withNds)
+                return ndsAmount * (cost * quantity)/100 + (cost * quantity);
+            else
+                return (cost * quantity);
+        }
+    }
+
+}
---
+++ Essensial2/Essensial2.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essensial2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(1, "Oleg", "Почта России");
+            invoice.Article = "Пистолет";
+            invoice.Quantity = 9;
+
+            invoice.ShowOrder(120);
+
+            Invoice invoice2 = new Invoice(1, "Marina", "Gruzovozov");
+            invoice2.Article = "Кукла";
+            invoice2.Quantity = 3;
+
+            invoice2.ShowOrder(98);
+
+            Console.ReadKey();
+        }
+    }
+}

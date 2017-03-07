4716a5ba4ff22c91cf5ca7539ff8e501e9ab9624
olegp306 <olegp306@gmail.com>
2017-03-07 12:45:48 +0700

    add home work Essential 2.3

---
+++ Essensial2/Essential2.3/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential2._3
+{
+    class Employee
+    {
+        string firstName;
+        string surName;
+
+        //public string salary;
+        public string position;
+        public int workExperience;
+        public double loan;
+
+        public Employee(string firstName, string surName)
+        {
+            this.firstName = firstName;
+            this.surName = surName;
+        }
+
+        private double getSalary(string position, int workExperience)
+        {
+            double salary = 0;
+            switch (position)
+            {
+                case "директор":
+                    salary = 1000 * workExperience * 1.1 * workExperience;
+                    break;
+                case "секретарь":
+                    salary = 200 * workExperience * 1.1 * workExperience;
+                    break;
+                case "рабочий":
+                    salary = 100 * workExperience * 1.1 * workExperience;
+                    break;
+                default:
+                    salary = 100;
+                    break;
+            }
+            return salary;
+        }
+
+        private double getLoanAmount(double salary)
+        {
+            return getLoanAmount(13,  salary);
+        }
+
+        private double getLoanAmount(double loan,double salary)
+        {
+            double loanAmount = 0;
+         
+            return loanAmount;
+        }
+
+        public void ShowEmployee(string position, int workExperience, double loan)
+        {
+            double salary = getSalary(position, workExperience);
+
+            Console.WriteLine("Имя:{0}", firstName);
+            Console.WriteLine("Фамилия:{0}", surName);
+            Console.WriteLine("Должность:{0}", position);
+            Console.WriteLine("Стаж:{0}", workExperience);
+            Console.WriteLine("Налог на ЗП:{0}", loan);
+            Console.WriteLine("Зарплата:{0}", salary);
+
+        }
+    }
+}
---
+++ Essensial2/Essential2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Oleg", "Panyukov");
+            employee.ShowEmployee("директор", 10, 13);
+
+            Employee employee2 = new Employee("Marina", "Fortuna");
+            employee2.ShowEmployee("секретарь", 5, 13);
+
+            Console.ReadKey();
+
+        }
+    }
+}

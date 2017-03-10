cab7b4f60b3b84f2d0cda55124bedc4caedc8d2b
olegp306 <olegp306@gmail.com>
2017-03-10 13:52:30 +0700

    essential  3.1 Finish

--- Essential3.1/Essential3.1/DataModel/ClassRoom.cs
+++ Essential3.1/Essential3.1/DataModel/ClassRoom.cs
@@ -8,30 +8,77 @@ namespace Essential3._1
 {
     public class ClassRoom
     {
-        Pupil pupil1;
-        Pupil pupil2;
-        Pupil pupil3;
+        Pupil[] pupils = new Pupil[3];
 
         public ClassRoom(Pupil pupil1, Pupil pupil2)
         {
-            this.pupil1 = pupil1;
-            this.pupil2 = pupil2;
-        }
-        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3)
-        {
-            this.pupil1 = pupil1;
-            this.pupil2 = pupil2;
-            this.pupil3 = pupil3;
+            this.pupils[0] = pupil1;
+            this.pupils[1] = pupil2;
+            this.pupils[2] = GenerateRndPupil();
         }
 
+        private Pupil GenerateRndPupil()
+        {
+            var rnd = new Random();
+            int rndPupil = rnd.Next(0, 2);
+            switch (rndPupil)
+            {
+                case 0:
+                    return new BadPupil();
+                case 1:
+                    return new GoodPupil();
+                case 2:
+                    return new ExcelentPupil();
+
+                default:return new BadPupil();
+            }
+        }
+
+        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3)
+        {
+            this.pupils[0] = pupil1;
+            this.pupils[1] = pupil2;
+            this.pupils[2] = pupil3;
+        }
+
+
+        void Study()
+        {
+            foreach (var pupil in pupils)
+            {
+                pupil.Study();
+            }
+        }
+        void Write()
+        {
+            foreach (var pupil in pupils)
+            {
+                pupil.Write();
+            }
+        }
+        void Read()
+        {
+            foreach (var pupil in pupils)
+            {
+                pupil.Read();
+            }
+        }
+        void Relax()
+        {
+            foreach (var pupil in pupils)
+            {
+                pupil.Relax();
+            }
+        }
 
         public void ShowClass()
         {
-            if(pupil1!=null)
-                pupil1.s
-                if (pupil1 != null)
-
-                    if (pupil1 != null)
+            Console.WriteLine();
+            Console.WriteLine("Результаты учеников класса");
+            Study();
+            Read();
+            Write();
+            Relax();
         }
 
     }
--- Essential3.1/Essential3.1/Program.cs
+++ Essential3.1/Essential3.1/Program.cs
@@ -10,6 +10,20 @@ namespace Essential3._1
     {
         static void Main(string[] args)
         {
+            BadPupil badPaupil = new BadPupil();
+            GoodPupil goodPupil = new GoodPupil();
+
+            ExcelentPupil excelentPupil = new ExcelentPupil();
+
+            ClassRoom class1 = new ClassRoom(badPaupil, goodPupil);
+            ClassRoom class2 = new ClassRoom(badPaupil, goodPupil, excelentPupil);
+
+            class1.ShowClass();
+            class2.ShowClass();
+
+
+            Console.ReadKey();
+
         }
     }
 }

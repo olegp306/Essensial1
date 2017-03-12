bb01171fc54b63df2bd6e9c4da6fb9c2d353bfdb
olegp306 <olegp306@gmail.com>
2017-03-12 20:22:09 +0600

    Home work Essential 4 Additional task. Add code from home work 12 to make new appoache by "absrtract class"

---
+++ HomeWorkEssential4/AdditionalTask/ProDocumentWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdditionalTask
+{
+    public class ProDocumentWorker:DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+}

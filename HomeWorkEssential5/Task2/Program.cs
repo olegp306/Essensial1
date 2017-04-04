db46eff68783735efc8be61d0f7f788f90a92440
olegp306 <olegp306@gmail.com>
2017-04-04 17:52:23 +0700

    finish Task2 in home work Essential5

---
+++ HomeWorkEssential5/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        /*Используя Visual Studio, создайте проект по шаблону Console Application.  
+        Требуется:  
+        Создать массив размерностью N элементов, заполнить его произвольными целыми значениями.  
+        Вывести наибольшее значение массива, наименьшее значение массива, общую сумму элементов, среднее арифметическое всех элементов, вывести все нечетные значения. 
+*/
+        static void Main(string[] args)
+        {
+
+            VariousArray myArr = new VariousArray(15);
+
+            myArr.ShowAll();
+            Console.WriteLine("Макс:{0}", myArr.FindMax());
+            Console.WriteLine("Мин:{0}", myArr.FindMin());
+            Console.WriteLine("Среднее ариф:{0}", myArr.ArithmeticMean());
+            Console.WriteLine("Общая сумма:{0}", myArr.GetSumAll());
+
+            Console.ReadKey();
+
+        }
+        
+    }
+   
+}
---
+++ HomeWorkEssential5/Task2/VariousArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class VariousArray
+    {
+        int[] arr;
+
+        public VariousArray(int sizeArray)
+        {
+            arr = new int[sizeArray];
+            Random rnd = new Random();
+
+            for (int i = 0; i < sizeArray ; i++)
+            {
+                arr[i] = rnd.Next(0, 50);
+            }
+        }
+
+        public int FindMax()
+        {
+            int max = arr[0];
+            foreach (var item in arr)
+            {
+                max = Math.Max(item, max);
+            }
+            return max;
+        }
+
+        public int FindMin()
+        {
+            int min = arr[0];
+            foreach (var item in arr)
+            {
+                min = Math.Min(item, min);
+            }
+            return min;
+        }
+
+        public double ArithmeticMean()
+        {
+            return GetSumAll() / arr.Length;
+        }
+
+        public int GetSumAll()
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        public void ShowAll()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

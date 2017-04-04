d444785c46f0b25ed62e71f8c38960f1aa38ffa7
olegp306 <olegp306@gmail.com>
2017-04-04 17:32:31 +0600

    add part of Task 3

---
+++ HomeWorkEssential5/Task3/MyMatrix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    // Создать класс MyMatrix, обеспечивающий представление матрицы произвольного размера
+    // с возможностью изменения числа строк и столбцов.
+    // Написать программу, которая выводит на экран матрицу и производные от нее матрицы разных порядков. 
+
+    class MyMatrix
+    {
+        int[,] matrix;
+
+        public MyMatrix(int x, int y)
+        {
+            matrix = new int[x, y];
+            fillMatrix();
+        }
+
+        void fillMatrix()
+        {
+            Random rnd = new Random();
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    matrix[x, y] = rnd.Next(10, 50);
+                }
+            }
+        }
+
+       public void ShowMatrix()
+        {
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    Console.Write(" {0}", matrix[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ HomeWorkEssential5/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMatrix matrix = new MyMatrix(15, 15);
+            matrix.ShowMatrix();
+            Console.WriteLine("Было еще что -то  с возможностью изменения числа строк и столбцов. Написать программу, которая выводит на экран матрицу и производные от нее матрицы разных порядков. ");
+            Console.WriteLine("Не стал не знаю о чем");
+            Console.ReadKey();
+        }
+    }
+}
